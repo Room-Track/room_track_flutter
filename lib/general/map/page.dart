56439e8aca907ef6f46631ddf3b4b6f2f841c807
Martin <martinigv123@gmail.com>
2024-10-14 22:31:51 -0300

    martin

--- lib/general/map/page.dart
+++ lib/general/map/page.dart
@@ -1,13 +1,112 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:room_track_flutter/colors.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  Location _locationController = new Location();
+
+  final Completer<GoogleMapController> _mapController = 
+    Completer();
+
+
+  static const LatLng _pGooglePlex = LatLng(-33.0349212872088, -71.59588228381082);
+  static const LatLng _pApplePark = LatLng(-33.02215257982356, -71.6336526057028);
+
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.black,
+  return Scaffold(
+    body: _currentP == null
+        ? const Center(
+            child: Text("Loading..."),
+        )
+      : GoogleMap(
+          onMapCreated: ((controller) => 
+              _mapController.complete(controller)),
+          initialCameraPosition: const CameraPosition(
+            target: _pGooglePlex,
+            zoom: 15,
+          ),
+          markers: {
+            Marker(
+              markerId: const MarkerId('_currentLocation'),
+              icon: BitmapDescriptor.defaultMarker,
+              position: _currentP!,
+              infoWindow: const InfoWindow(title: "Your Location"),
+            ),
+            const Marker(
+              markerId: MarkerId('_sourceLocation'),
+              icon: BitmapDescriptor.defaultMarker,
+              position: _pGooglePlex,
+              infoWindow: InfoWindow(title: "Start"),
+            ),
+            const Marker(
+              markerId: MarkerId('_destionationLocation'),
+              icon: BitmapDescriptor.defaultMarker,
+              position: _pApplePark,
+              infoWindow: InfoWindow(title: "To Go"),
+          ),
+        },
+      ),
     );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition = CameraPosition(
+      target: pos,
+      zoom: 15,
+    );
+    await controller.animateCamera(
+    CameraUpdate.newCameraPosition(_newCameraPosition),
+    );
+  }
+
+  Future<void> getLocationUpdates()async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if(_serviceEnabled){
+      _serviceEnabled = await _locationController.requestService();
+    }else{
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await _locationController.requestPermission();
+      if(_permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation){
+      if(currentLocation.latitude != null && 
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP = 
+              LatLng(currentLocation.latitude!,currentLocation.longitude! );
+          _cameraToPosition(_currentP!);
+        });
+      }
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import location
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -15,7 +16,8 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
